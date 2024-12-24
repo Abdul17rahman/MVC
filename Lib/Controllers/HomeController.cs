09500fb07a8b3b3fe12b0586a5cf1ffcaa4b8387
Abdul Rahman <abdulnsamba@gmail.com>
2024-12-24 10:38:19 +0300

    Add project files.

---
+++ Lib/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lib.Models;
+
+namespace Lib.Controllers
+{
+    public class HomeController : Controller
+    {
+        Lib_dbEntities _LibEFObj = new Lib_dbEntities();
+        public ActionResult Index()
+        {
+            var indexdata = _LibEFObj.Bookstbls.ToList();
+            return View(indexdata);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Bookstbl frommodel)
+        {
+            if (ModelState.IsValid)
+            {
+                _LibEFObj.Bookstbls.Add(frommodel);
+                _LibEFObj.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else 
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var book = _LibEFObj.Bookstbls.Find(id);
+
+            if (book == null)
+            {
+                ModelState.AddModelError("", "The book you are trying to edit does not exist.");
+                return RedirectToAction("Index");
+            }
+            return View(book);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Bookstbl book)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var foundBook = _LibEFObj.Bookstbls.Find(book.BID);
+
+                    if (foundBook != null)
+                    {
+                        foundBook.BTitle = book.BTitle;
+                        foundBook.Author = book.Author;
+                        foundBook.Category = book.Category;
+                        foundBook.Copies = book.Copies;
+                        foundBook.Publisher = book.Publisher;
+                        foundBook.PublishDate = book.PublishDate;
+
+                        _LibEFObj.SaveChanges();
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "The book could not be found for editing.");
+                        return View(book);
+                    }
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "An error occurred while saving the book: " + ex.Message);
+                    return View(book);
+                }
+            }
+
+            return View(book);
+        }
+    }
+}
---
+++ Products/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Products.Models;
+
+namespace Products.Controllers
+{
+    public class HomeController : Controller
+    {
+        products_dbEntities _dbEFOnject = new products_dbEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var indexdata = _dbEFOnject.products.ToList();
+            return View(indexdata);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(product frommodel)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbEFOnject.products.Add(frommodel);
+                _dbEFOnject.SaveChanges();
+
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            var data = _dbEFOnject.products.Where(y=>y.PID == id).FirstOrDefault();
+
+            return View(data);
+        }
+    }
+}
