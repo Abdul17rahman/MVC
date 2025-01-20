a9b9d179efe49f05d52846d15bfaa8cde9e3e3d0
Abdul Rahman <abdulnsamba@gmail.com>
2025-01-20 18:18:19 +0300

    added login

---
+++ ass_4/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ass_4.Model;
+
+namespace ass_4.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        CarsEntities1 carsdb = new CarsEntities1();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                var user = carsdb.users.SingleOrDefault(u => u.username == username && u.password == password);
+                if (user == null)
+                {
+                    ModelState.AddModelError("LoginError", "Invalid username or password.");
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View();
+        } 
+    }
+}
---
+++ ass_4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ass_4.MvcApplication" Language="C#" %>
